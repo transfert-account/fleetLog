76f1962d41c0d745545817ba510b9eedc3ab9f35
Maxime <maxime.poulet@ynov.com>
2020-02-09 21:45:38 +0100

    fixed nuke

--- imports/startup/api/test/resolvers.js
+++ imports/startup/api/test/resolvers.js
@@ -4,16 +4,14 @@ export default {
     Query : {
         testThis(obj, args,{user}){
             Vehicles.update(
-                {
-                    _id: new Mongo.ObjectID(_id)
-                }, {
+                {}, {
                     $set: {
                         "archived":false,
                         "archiveReason":"",
                         "archiveDate":""
                     }
                 }
-            ); 
+            );
             return "true";
         }
     }
