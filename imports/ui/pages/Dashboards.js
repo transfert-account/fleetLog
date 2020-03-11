46383e8c25d2bb24e19223bceb37730d286dd150
Maxime <maxime.poulet@ynov.com>
2020-03-11 02:08:24 +0100

    Fixed typo

--- imports/ui/pages/Dashboards.js
+++ imports/ui/pages/Dashboards.js
@@ -103,7 +103,7 @@ class Dashboard extends Component {
         if(this.state.dataLoaded && this.state.societeLoaded){
             return (
                 <div style={{display:"grid",gridTemplateColumns:"1fr 6fr 1fr",gridTemplateRows:"auto auto 1fr",gridGap:"16px"}}>
-                    <Header style={{placeSelf:"center",gridColumnEnd:"span 3"}} as="h1">Tableaud de bord : {this.state.dashboardsRaw.filter(d=>d.societe._id == this.props.societeFilter)[0].societe.name}</Header>
+                    <Header style={{placeSelf:"center",gridColumnEnd:"span 3"}} as="h1">Tableau de bord : {this.state.dashboardsRaw.filter(d=>d.societe._id == this.props.societeFilter)[0].societe.name}</Header>
                     {this.state.dashboard()}
                 </div>
             )
