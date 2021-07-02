e7dd11d0e7d5d203fba9c9b964502d10a796c10c
Maxime <maxime.poulet.4593@gmail.com>
2021-07-02 22:07:39 +0200

    Another fix

--- imports/startup/api/vehicle/resolvers.js
+++ imports/startup/api/vehicle/resolvers.js
@@ -37,7 +37,7 @@ const affectVehicleControls = vehicle => {
             }
         }else{
             return {
-                control:o,
+                control:control,
                 selected:false
             }
         }
